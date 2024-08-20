b23c0639f58c821ae2e0ee1dad593bf4e8a6a774
cursos-careri <cursos.careri@gmail.com>
2024-08-20 08:03:28 -0600

    Update REFs.py

--- REFs.py
+++ REFs.py
@@ -1,2 +1,4 @@
 # Dirección de referencia: https://github.com/cursos-careri
 # Encontrar cambios y editar los accesos
+# Establecer prioridades para generar actualizaciones
+# Dar seguimiento a actualizaciones
