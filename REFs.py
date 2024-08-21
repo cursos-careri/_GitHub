f661ebb137fb04be5aaec5b92b0ae9df0e509516
cursos-careri <cursos.careri@gmail.com>
2024-08-21 08:18:12 -0600

    Actualización miércoles

--- REFs.py
+++ REFs.py
@@ -1,4 +1,9 @@
 # Dirección de referencia: https://github.com/cursos-careri
+#
+# Sitios locales:
+#   https://github.com/cursos-careri/_GitHub/tree/main/uPs-y-uCs
+#   https://github.com/cursos-careri/_GitHub/tree/main/ProgInterfaces
+#
 # Encontrar cambios y editar los accesos
 # Establecer prioridades para generar actualizaciones
 # Dar seguimiento a actualizaciones
